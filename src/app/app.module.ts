098974af71caa1f8c769e0dd6db55559bd56ce3d
polo dev <polodev10@gmail.com>
2017-07-25 12:28:00 +0600

    added routerj

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {RouterModule} from '@angular/router'
 
 import { AppComponent } from './app.component';
 import { MoviesListComponent } from './components/movies-list/movies-list.component';
@@ -12,7 +13,17 @@ import { CharactersListComponent } from './components/characters-list/characters
     CharactersListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+        {
+          'path' : 'movies',
+          component: MoviesListComponent
+        },
+        {
+          'path' : 'characters',
+          component: CharactersListComponent
+        },
+      ])
   ],
   providers: [],
   bootstrap: [AppComponent]
